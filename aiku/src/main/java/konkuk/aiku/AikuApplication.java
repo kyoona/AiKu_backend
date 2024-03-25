204344245ba0eb379514762a9aaf8da313bea801
kyoona <yoona405@konkuk.ac.kr>
2024-03-25 15:07:06 +0900

    프로젝트 환경 설정

---
+++ aiku/src/main/java/konkuk/aiku/AikuApplication.java
@@ -0,0 +1,13 @@
+package konkuk.aiku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikuApplication.class, args);
+	}
+
+}
