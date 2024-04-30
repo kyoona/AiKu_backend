ecc4e27dbc5f9108b3c9e289862062fbfe79b9a6
kyoona <yoona405@konkuk.ac.kr>
2024-05-01 04:20:04 +0900

    [controller, resources] 관리자 페이지 초기 화면

---
+++ aiku/src/main/java/konkuk/aiku/controller/admin/AdminController.java
@@ -0,0 +1,24 @@
+package konkuk.aiku.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "login";
+    }
+
+    @PostMapping("/")
+    public String login(){
+        return "redirect:main";
+    }
+
+    @RequestMapping("/main")
+    public String mainPage(){
+        return "main";
+    }
+}
