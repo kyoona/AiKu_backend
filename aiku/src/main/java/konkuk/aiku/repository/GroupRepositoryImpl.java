fdb85d44af4e58262d86466a4b1708331f433228
kyoona <yoona405@konkuk.ac.kr>
2024-04-30 15:07:48 +0900

    [repository] repository클래스 기본 설계 (@Repository 추가)

--- aiku/src/main/java/konkuk/aiku/repository/GroupRepositoryImpl.java
+++ aiku/src/main/java/konkuk/aiku/repository/GroupRepositoryImpl.java
@@ -1,4 +1,7 @@
 package konkuk.aiku.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class GroupRepositoryImpl implements GroupRepository{
 }
--- aiku/src/main/java/konkuk/aiku/repository/UserRepositoryImpl.java
+++ aiku/src/main/java/konkuk/aiku/repository/UserRepositoryImpl.java
@@ -1,4 +1,7 @@
 package konkuk.aiku.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepositoryImpl implements UserRepository{
 }
