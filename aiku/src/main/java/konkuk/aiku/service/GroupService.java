d90695892d7f1d6e4c29405c12164a16cae1e967
곽유나 <yoona405@konkuk.ac.kr>
2024-05-10 05:47:21 +0900

    feat : 그룹 요청 성공 응답메세지에 요청 그룹 아이디 추가

--- aiku/src/main/java/konkuk/aiku/service/GroupService.java
+++ aiku/src/main/java/konkuk/aiku/service/GroupService.java
@@ -51,7 +51,7 @@ public class GroupService {
     }
 
     @Transactional
-    public void modifyGroup(Users user, Long groupId, GroupServiceDto groupServiceDTO) {
+    public Long modifyGroup(Users user, Long groupId, GroupServiceDto groupServiceDTO) {
         Groups group = checkUserInGroup(user.getId(), groupId).getGroup();
         if(StringUtils.hasText(groupServiceDTO.getGroupName())){
             group.setGroupName(groupServiceDTO.getGroupName());
@@ -62,15 +62,17 @@ public class GroupService {
         if(StringUtils.hasText(groupServiceDTO.getGroupImg())){
             group.setGroupImg(groupServiceDTO.getGroupImg());
         }
+        return group.getId();
     }
 
     @Transactional
-    public void deleteGroup(Users user, Long groupId) {
+    public Long deleteGroup(Users user, Long groupId) {
         Long userId = user.getId();
         checkUserInGroup(userId, groupId);
 
         userGroupRepository.deleteByUserIdAndGroupId(userId, groupId);
         groupsRepository.deleteById(groupId);
+        return groupId;
     }
 
     public GroupDetailServiceDto findGroupDetailById(Users user, Long groupId) {
@@ -85,7 +87,7 @@ public class GroupService {
     }
 
     @Transactional
-    public void enterGroup(Users user, Long groupId){
+    public Long enterGroup(Users user, Long groupId){
         Groups group = findGroupById(groupId);
 
         UserGroup userGroup = UserGroup.builder()
@@ -93,13 +95,15 @@ public class GroupService {
                 .group(group)
                 .build();
         userGroupRepository.save(userGroup);
+        return groupId;
     }
 
     @Transactional
-    public void exitGroup(Users user, Long groupId){
+    public Long exitGroup(Users user, Long groupId){
         Long userId = user.getId();
         checkUserInGroup(userId, groupId);
         userGroupRepository.deleteByUserIdAndGroupId(userId, groupId);
+        return groupId;
     }
 
     public Groups findGroupById(Long groupId){
