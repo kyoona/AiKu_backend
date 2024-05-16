0f574423ef96c3b26d88c62f35818d890d2e78bf
곽유나 <yoona405@konkuk.ac.kr>
2024-05-16 22:59:50 +0900

    feat : 스케줄 등록시 스케줄 전날, 당일 알람 등록

--- aiku/src/main/java/konkuk/aiku/repository/ScheduleRepository.java
+++ aiku/src/main/java/konkuk/aiku/repository/ScheduleRepository.java
@@ -1,7 +1,14 @@
 package konkuk.aiku.repository;
 
 import konkuk.aiku.domain.Schedule;
+import konkuk.aiku.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom{
+    @Query("SELECT us.user FROM UserSchedule us WHERE us.schedule.id = :scheduleId")
+    List<Users> findUsersByScheduleId(@Param("scheduleId") Long scheduleId);
 }
