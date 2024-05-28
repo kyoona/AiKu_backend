dc88490636dffd97d60e6e40fca7312e10f1d675
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-05-28 22:57:07 +0900

    feat: 칭호 부여 정책 추가

--- aiku/src/main/java/konkuk/aiku/repository/BettingRepository.java
+++ aiku/src/main/java/konkuk/aiku/repository/BettingRepository.java
@@ -2,6 +2,7 @@ package konkuk.aiku.repository;
 
 import konkuk.aiku.domain.Betting;
 import konkuk.aiku.domain.BettingType;
+import konkuk.aiku.domain.ResultType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,4 +13,11 @@ public interface BettingRepository extends JpaRepository<Betting, Long> {
 
     @Query("select b from Betting b where b.schedule.id = ?1 and b.bettor.id = ?2")
     List<Betting> findBettingsByScheduleIdAndBettorId(Long scheduleId, Long userId);
+
+    @Query("select count(b) from Betting b where b.bettor.id = ?1 and b.resultType = ?2")
+    long countByBettorIdAndResultType(Long userId, ResultType resultType);
+
+    @Query("select count(b) from Betting b where b.targetUser.id = ?1 and b.resultType = ?2")
+    long countByTargetUserIdAndResultType(Long userId, ResultType resultType);
+
 }
