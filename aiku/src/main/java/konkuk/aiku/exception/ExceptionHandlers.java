e6efadeeaeed0a5ab32b6553eaea7c191f64ac3e
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-06-02 04:03:34 +0900

    feat: Security에서 ExceptionHandler가 적용되지 않던 문제 해결

--- aiku/src/main/java/konkuk/aiku/exception/ExceptionHandlers.java
+++ aiku/src/main/java/konkuk/aiku/exception/ExceptionHandlers.java
@@ -1,8 +1,12 @@
 package konkuk.aiku.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -40,6 +44,27 @@ public class ExceptionHandlers  {
     public ResponseEntity<ErrorDTO> voiceException(VoiceApiException e){
         log.error("ERROR", e);
         return new ResponseEntity<>(new ErrorDTO(e.getErrorCode().getCode(), e.getErrorCode().getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // 아래 내용 모두 추가
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<ErrorDTO> handleSignatureException() {
+        return new ResponseEntity<>(new ErrorDTO(NOT_AVAILABLE_TOKEN.getCode(), NOT_AVAILABLE_TOKEN.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<ErrorDTO> handleMalformedJwtException() {
+        return new ResponseEntity<>(new ErrorDTO(WRONG_TOKEN_TYPE.getCode(), WRONG_TOKEN_TYPE.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorDTO> handleExpiredJwtException() {
+        return new ResponseEntity<>(new ErrorDTO(EXPIRED_TOKEN.getCode(), EXPIRED_TOKEN.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorDTO> badCredentialsException() {
+        return new ResponseEntity<>(new ErrorDTO(BAD_CREDENTIALS.getCode(), BAD_CREDENTIALS.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler({Exception.class})
