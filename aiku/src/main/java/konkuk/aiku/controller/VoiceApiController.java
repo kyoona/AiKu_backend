9edebf32a36d2d5735c09fef76ddbe4f74d5414d
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-06-02 01:10:24 +0900

    feat: VoiceApi 완성

--- aiku/src/main/java/konkuk/aiku/controller/VoiceApiController.java
+++ aiku/src/main/java/konkuk/aiku/controller/VoiceApiController.java
@@ -11,13 +11,15 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 public class VoiceApiController {
     private final VoiceApiService voiceApiService;
 
     @GetMapping("/voice")
-    public ResponseEntity<VoiceResponseDto> voiceToData(@RequestPart MultipartFile file) {
+    public ResponseEntity<VoiceResponseDto> voiceToData(@RequestPart MultipartFile file) throws IOException {
         VoiceApiResponseDto voiceApiResponseDto = voiceApiService.voiceToData(file);
         return new ResponseEntity<>(voiceApiResponseDto.toResponseDto(), HttpStatus.OK);
     }
--- aiku/src/main/java/konkuk/aiku/service/dto/VoiceApiResponseDto.java
+++ aiku/src/main/java/konkuk/aiku/service/dto/VoiceApiResponseDto.java
@@ -1,16 +1,19 @@
 package konkuk.aiku.service.dto;
 
 import konkuk.aiku.controller.dto.VoiceResponseDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @Builder
 public class VoiceApiResponseDto {
     private String DT; // 날짜
     private String TI; // 시간
     private String OG; // 장소
-    private String status;
+    private String STATUS;
 
     public VoiceResponseDto toResponseDto() {
         return VoiceResponseDto.builder()
