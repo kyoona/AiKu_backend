24606ec430f9b170af2833909fcbdf847226ac51
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-06-02 05:15:19 +0900

    fix: ImgType Default0 추가

--- aiku/src/test/java/konkuk/aiku/service/VoiceApiTest.java
+++ aiku/src/test/java/konkuk/aiku/service/VoiceApiTest.java
@@ -6,16 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
-@SpringBootTest
-@Transactional
-public class VoiceApiTest {
-
-    @Autowired
-    VoiceApiService voiceApiService;
-
-    @Test
-    void test() {
-        voiceApiService.test();
-    }
-}
+//@Slf4j
+//@SpringBootTest
+//@Transactional
+//public class VoiceApiTest {
+//
+//    @Autowired
+//    VoiceApiService voiceApiService;
+//
+//    @Test
+//    void test() {
+//        voiceApiService.test();
+//    }
+//}
